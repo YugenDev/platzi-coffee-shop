a7a414662ee42c1f49a7ed095618a0f56e5ba280
yugoso8512 <yugoso8512@gmail.com>
2024-07-04 21:50:11 +0000

    si

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'coffee_shop.urls'
+ROOT_URLCONF = 'mysite.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'coffee_shop.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
 # Database
